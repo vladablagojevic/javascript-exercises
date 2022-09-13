016ad08b206d07bf0af10af7b769df97f09dbece
Vladimir Blagojevic <blagojevic.vlada@gmail.com>
2022-09-13 15:07:09 +0200

    Done with first and second exercises
    Passed the first and the simplest test helloWorld. Test enviroment
    is set correctly.
    Passed all tests in the second exercise repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,35 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let repeatedString = '';
+  
+  while (num > 0) {
+    repeatedString += string;
+  num--;
+  }
 
+  if (num < 0) {
+    return "ERROR";
+  }
+  
+  return repeatedString;
 };
+
+// function repeatString(string, num) {
+//   if num < 0) 
+//     return "";
+//   if num === 1) 
+//     return string;
+//   else 
+//     return string + repeatString(string, num - 1);
+// }
+
+// function repeatString(string, num) {
+//   if  num > 0)
+//     return string.repeat num);
+//   else
+//     return "";
+// }
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
