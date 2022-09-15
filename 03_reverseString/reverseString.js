1e5eb092f45b1858e7e16e0a6a35cae3b20d9b0f
Vladimir Blagojevic <blagojevic.vlada@gmail.com>
2022-09-15 22:30:48 +0200

    Passed third and fourth tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  return string.split("").reverse().join("");
+}
 
-};
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr, num) {
+//   const arg1 = Array.from(arr);
+//   const index = arg1.indexOf(num);
+//   if (index > -1) {
+//     arg1.splice(index, 1);
+//   }
+//   return arg1;
+// }
 
-};
+// removeFromArray([1, 2, 3, 4], 3);
+
+// const removeFromArray = function(array, ...args) {
+//   return array.filter(x => !args.includes(x));
+// }
+
+const removeFromArray = function(array, ...args) {
+  args.forEach((arg) => {
+    const index = array.indexOf(arg);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  });
+  return array;
+}
+
+console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
