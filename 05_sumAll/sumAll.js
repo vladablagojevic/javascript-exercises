da02c988a67cbe84c3a90a6f6a91e7df8e999c56
Vladimir Blagojevic <blagojevic.vlada@gmail.com>
2022-09-16 18:16:54 +0200

    Passed all tests in 05_sumAll folder

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  // if (typeof num1 != Number || typeof num2 != Number) {
+  //   return "ERROR";
+  // }
+
+  let lower = Math.min(num1, num2);
+  let upper = Math.max(num1, num2);
+
+  let total = 0;
+  
+  for (let i = lower; i <= upper; i++) {
+    total += i;
+  }
+
+  if (total < 0) {
+    return "ERROR";
+  }
+
+  return total;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
